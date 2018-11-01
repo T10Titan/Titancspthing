3c23a459a4d555eb7bb15d3161e8ae6c1c5052e8
Trujillo <ttru4447@740s-pl-129.local>
2018-11-01 08:12:24 -0600

    Initial commit

---
+++ Titancspthing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Titancspthing
+//
+//  Created by Trujillo, Teyton on 11/1/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
