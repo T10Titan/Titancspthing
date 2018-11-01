5365bc28db27133d57bc378b1bacb8548235642a
Trujillo <ttru4447@740s-pl-129.local>
2018-11-01 08:39:24 -0600

    fixed error

---
+++ Titancspthing/Controller/DrawingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Titancspthing
+//
+//  Created by Trujillo, Teyton on 11/1/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController
+{
+
+        public override func viewDidLoad() -> Void
+        {
+            super.viewDidLoad()
+        }
+
+}
+
